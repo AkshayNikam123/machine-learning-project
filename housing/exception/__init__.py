b966e445c8c70e0cd8a96aed73b18a07cbb57616
Akshay Nikam <nikam.ak152@gmail.com>
2023-04-04 16:00:19 +0530

    updated

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -1,8 +1,8 @@
 import os
-import sys
+import sys  #inherite exception class present first using os sys then custom exception for ur projrct
 
 class HousingException(Exception):
-    
+    # error_message is object for exception class
     def __init__(self, error_message:Exception,error_detail:sys):#sys module used to execution details and  declare which line,file causing error
         super().__init__(error_message) #super is parent class initlaiser i.e exception u can write exception(error_message)
         self.error_message=HousingException.get_detailed_error_message(error_message=error_message,
@@ -16,7 +16,7 @@ class HousingException(Exception):
         error_message: Exception object
         error_detail: object of sys module
         """
-        _,_ ,exec_tb = error_detail.exc_info()  # _,_ blank spaces for first two value which we dont want from exc_info()
+        _,_ ,exec_tb = error_detail.exc_info()  # _,_ blank spaces for type and value which we dont want from exc_info() #exc_info gives recent exception
         exception_block_line_number = exec_tb.tb_frame.f_lineno
         try_block_line_number = exec_tb.tb_lineno
         file_name = exec_tb.tb_frame.f_code.co_filename
@@ -28,9 +28,9 @@ class HousingException(Exception):
         """
         return error_message
 
-    def __str__(self):
+    def __str__(self): # how object for class should be represent in print 
         return self.error_message
 
 
-    def __repr__(self) -> str:
+    def __repr__(self) -> str:#how object for class should be represent without print
         return HousingException.__name__.str()
--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup,find_packages
 from typing import List
-
+#all data type comes from typing library
 
 #declaring variables for setup functions
 project_name="house-predictor"
-Version="0.0.1"  #any version u want 
+Version="0.0.1"  #any version u want and change version name  after every modify
 AUTHOR='Akshay Nikam'
 DESCRIPTION='This is a my first ml project'
 PACKAGES=find_packages()  # hard coded with ['housing'] pacakage but will create problem later so better use find package for dynamic
 #inside housing we are creating package so that find package will take that files which contain init main as a package
 REQUIREMENT_FILE_NAME="requirements.txt"
-def get_requirements_list()->List[str]: #list just define what is that function going return so import list
+def get_requirements_list()->List[str]: #list[str] just define what is that function going return but first import list
     """
     Description: This function is going to return list of requirement
     mention in requirements.txt file
@@ -18,7 +18,7 @@ def get_requirements_list()->List[str]: #list just define what is that function 
     of libraries mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        requirement_file.readlines()
+        requirement_file.readlines().remove("-e .")
 
 
 setup(
@@ -26,7 +26,7 @@ setup(
     version=Version,
     author=AUTHOR,
     description=DESCRIPTION,
-    packages=PACKAGES,
+    packages=find_packages(), #find packages does the same thing -e. thats why we remove in setup.py as above
     install_requires=get_requirements_list()
 
 )
